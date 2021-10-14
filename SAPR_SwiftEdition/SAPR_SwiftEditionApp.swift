acd1897c01b95a264eed8cecf7fcb6836a785fa1
AginS <agins.main@gmail.com>
2021-10-14 16:53:34 +0300

    Initial commit

---
+++ SAPR_SwiftEdition/SAPR_SwiftEditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAPR_SwiftEditionApp.swift
+//  SAPR_SwiftEdition
+//
+//  Created by Vlad Vrublevsky on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SAPR_SwiftEditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
