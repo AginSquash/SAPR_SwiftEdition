34d67024f92b54a9ecae9000fcec7249ae77fc35
AginS <agins.main@gmail.com>
2021-10-21 01:56:24 +0300

    Now this works!

--- SAPR_SwiftEdition/Views/Loads/AddLoadView.swift
+++ SAPR_SwiftEdition/Views/Loads/AddLoadView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct AddLoadView: View {
+    static let column_width: CGFloat? = 125
+    let id_range: [Int]
+    
+    @State private var selectedID: Int = 0
+    @State private var enteredLoad: String = ""
     var body: some View {
-        Text("")
+        VStack {
+            HStack {
+                Text("Номер стержня")
+                    .frame(width: AddLoadView.column_width)
+                Text("Распределенная нагрузка")
+                    .frame(width: AddLoadView.column_width)
+            }
+            HStack {
+                Picker(selection: $selectedID, label: Text("")) {
+                    ForEach(0..<id_range.count) { i in
+                        Text("\(id_range[i])").tag(i)
+                    }
+                }
+                .frame(width: AddLoadView.column_width)
+                
+                TextField("", text: $enteredLoad)
+                    .frame(width: AddLoadView.column_width)
+            }
+        }
+        .multilineTextAlignment(.center)
+        .padding()
     }
 }
 
 struct AddLoadView_Previews: PreviewProvider {
     static var previews: some View {
-        AddLoadView()
+        AddLoadView(id_range: [1, 2, 3])
     }
 }
