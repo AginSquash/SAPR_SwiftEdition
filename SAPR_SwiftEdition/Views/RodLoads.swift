6193445b3d0398a05dbdaa4881d402145d2ec33c
AginS <agins.main@gmail.com>
2021-10-20 23:28:01 +0300

    Create RodLoads.swift

---
+++ SAPR_SwiftEdition/Views/RodLoads.swift
@@ -0,0 +1,20 @@
+//
+//  RodLoads.swift
+//  SAPR_SwiftEdition
+//
+//  Created by Vlad Vrublevsky on 20.10.2021.
+//
+
+import SwiftUI
+
+struct RodLoads: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RodLoads_Previews: PreviewProvider {
+    static var previews: some View {
+        RodLoads()
+    }
+}
