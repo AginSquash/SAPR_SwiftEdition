d6abc1b47e246c3687906f7afdfb501ad5f5c163
AginS <agins.main@gmail.com>
2021-10-21 00:50:50 +0300

    Create AddLoadView.swift

---
+++ SAPR_SwiftEdition/Views/Loads/AddLoadView.swift
@@ -0,0 +1,20 @@
+//
+//  AddLoadView.swift
+//  SAPR_SwiftEdition
+//
+//  Created by Vlad Vrublevsky on 21.10.2021.
+//
+
+import SwiftUI
+
+struct AddLoadView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct AddLoadView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddLoadView()
+    }
+}
