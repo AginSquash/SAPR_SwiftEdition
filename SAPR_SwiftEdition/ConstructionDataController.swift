bb5de2084b9fdc6d81a7dcc0b58f4fed45763671
AginS <agins.main@gmail.com>
2021-10-21 01:18:56 +0300

    now inMemory work correct

--- SAPR_SwiftEdition/ConstructionDataController.swift
+++ SAPR_SwiftEdition/ConstructionDataController.swift
@@ -11,8 +11,13 @@ class ConstructionDataController: ObservableObject {
     @Published var Rods: [Rod]
     @Published var Loads: [LinearLoad]
     init(inMemory: Bool = false) {
-        self.Rods = []
-        self.Loads = []
+        if inMemory {
+            self.Rods = [Rod(id: 1, L: 10, A: 2.1, E: 1.1, Sigma: 1.5), Rod(id: 2, L: 20, A: 61, E: 31, Sigma: 1.5)]
+            self.Loads = [LinearLoad(id: 1, q: 10), LinearLoad(id: 2, q: -20)]
+        } else {
+            self.Rods = []
+            self.Loads = []
+        }
     }
     
     static var preview: ConstructionDataController = {
