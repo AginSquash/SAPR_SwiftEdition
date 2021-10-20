564a914cf08de319fd02f79c60cf9a1a6c7f7af5
AginS <agins.main@gmail.com>
2021-10-21 02:40:53 +0300

    Added saving & alert

--- SAPR_SwiftEdition/Views/Loads/AddLoadView.swift
+++ SAPR_SwiftEdition/Views/Loads/AddLoadView.swift
@@ -8,19 +8,32 @@
 import SwiftUI
 
 struct AddLoadView: View {
-    static let column_width: CGFloat? = 125
-    let id_range: [Int]
+    struct AlertType: Identifiable {
+        let id = UUID()
+        let isError: Bool
+        let text: String
+    }
     
-    @State private var selectedID: Int = 0
+    @Environment(\.presentationMode) var presentationMode
+    static let column_width: CGFloat? = 125
+    
+    let id_range: [Int]
+    @ObservedObject var ConctructionDC: ConstructionDataController
+    
+    @State private var selectedID: Int = -1
     @State private var enteredLoad: String = ""
+    @State private var alert: AlertType? = nil
+    
     var body: some View {
         VStack {
+            
             HStack {
                 Text("Номер стержня")
                     .frame(width: AddLoadView.column_width)
                 Text("Распределенная нагрузка")
                     .frame(width: AddLoadView.column_width)
             }
+            
             HStack {
                 Picker(selection: $selectedID, label: Text("")) {
                     ForEach(0..<id_range.count) { i in
@@ -32,14 +45,51 @@ struct AddLoadView: View {
                 TextField("", text: $enteredLoad)
                     .frame(width: AddLoadView.column_width)
             }
+            
+            HStack {
+                Button("Decline") {
+                    alert = AlertType(isError: false, text: "Вы уверены что хотите отменить все изменения?")
+                }
+                Button("Save") {
+                    save()
+                }
+            }
+            .padding(.top)
         }
         .multilineTextAlignment(.center)
         .padding()
+        .alert(item: $alert) { alert in
+            if alert.isError {
+                return Alert(title: Text("Error"), message: Text(alert.text), dismissButton: .cancel())
+            } else {
+                return Alert(title: Text("Alert"), message: Text(alert.text), primaryButton: .destructive(Text("Выйти без сохранения"), action: { closeView() }), secondaryButton: .cancel() )
+            }
+        }
+    }
+    
+    func save() {
+        guard selectedID != -1 else {
+            alert = AlertType(isError: true, text: "Выберите стержень")
+            return
+        }
+        
+        guard let wrapped_value = Float(enteredLoad) else {
+            alert = AlertType(isError: true, text: "Вводить можно только цифры")
+            return
+        }
+        
+        ConctructionDC.Loads.append(LinearLoad(id: id_range[selectedID], q: wrapped_value))
+        
+        closeView()
+    }
+    
+    func closeView() {
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
 struct AddLoadView_Previews: PreviewProvider {
     static var previews: some View {
-        AddLoadView(id_range: [1, 2, 3])
+        AddLoadView(id_range: [1, 2, 3], ConctructionDC: .preview)
     }
 }
