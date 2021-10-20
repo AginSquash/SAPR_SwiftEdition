a32995daf35909332688efd56843e80956ff8955
AginS <agins.main@gmail.com>
2021-10-20 14:00:38 +0300

    Added main menu

---
+++ SAPR_SwiftEdition/Views/MainView.swift
@@ -0,0 +1,36 @@
+//
+//  MainView.swift
+//  SAPR_SwiftEdition
+//
+//  Created by Vlad Vrublevsky on 20.10.2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink("RodTableView", destination: RodTableView())
+                NavigationLink("ContentView", destination: ContentView())
+            }
+            Text("Select a Note")
+        }.toolbar {
+            ToolbarItem(placement: .navigation) {
+                Button(action: toggleSidebar, label: {
+                    Image(systemName: "sidebar.leading")
+                })
+            }
+        }
+    }
+    
+    private func toggleSidebar() {
+        NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
