8f5c33bbdd089df02a276171f48ffc81a701fcf4
AginS <agins.main@gmail.com>
2021-10-26 12:09:42 +0300

    Create ContructionSupport.swift

---
+++ SAPR_SwiftEdition/ContructionSupport.swift
@@ -0,0 +1,24 @@
+//
+//  ContructionSupport.swift
+//  SAPR_SwiftEdition
+//
+//  Created by Vlad Vrublevsky on 26.10.2021.
+//
+
+import Foundation
+
+enum SupportType: Int, Codable {
+    case Left = 0
+    case Right = 1
+    case Both = 2
+    
+    init?(rawValue: Int) {
+        switch rawValue {
+        case 0: self = .Left
+        case 1: self = .Right
+        case 2: self = .Both
+        default:
+            self = .Left
+        }
+    }
+}
